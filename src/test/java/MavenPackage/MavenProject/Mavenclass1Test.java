a3d4e288a07626b72797366c613c3a877a197adc
gaurav <beinggauravmalik@gmail.com>
2020-04-18 16:49:59 +0530

    first commit

---
+++ src/test/java/MavenPackage/MavenProject/Mavenclass1Test.java
@@ -0,0 +1,23 @@
+package MavenPackage.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class Mavenclass1Test {
+	
+	@Test
+	
+	public void firstmethod () {
+		
+		System.out.println("firstmethod");
+	}
+	
+@Test
+	
+	public void secondmethod () {
+		
+		System.out.println("secondmethod");
+	}
+	
+	
+
+}
---
+++ src/test/java/MavenPackage/MavenProject/Mavenclass2Test.java
@@ -0,0 +1,23 @@
+package MavenPackage.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class Mavenclass2Test {
+	
+	
+	@Test
+	
+	public void thirdmethod () {
+		
+		System.out.println("thirdmethod");
+	}
+	
+@Test
+	
+	public void fourthmethod () {
+		
+		System.out.println("fourthmethod");
+	}
+	
+
+}
